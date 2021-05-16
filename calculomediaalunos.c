3fc2a8231409d1a5c2f5fe5bbadacbf118ca9dc8
leiltonramos <leiltonr@outlook.com>
2021-05-16 09:17:01 -0300

    Calculo_Media

---
+++ calculomediaalunos.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int contador = 1, alunos;
+    float total = 0, media, nota;
+
+    printf("Digite a quantidade de alunos:\n>");
+    scanf("%d", &alunos);
+
+    while(contador <= alunos){
+        printf("Digite a nota:\n>");
+        scanf("%f", &nota);
+        total = total + nota;
+        contador++;    
+    }
+
+    media = total / alunos;
+
+    printf("A media da sala eh: %f\n", media);
+
+
+    return(0);
+}
