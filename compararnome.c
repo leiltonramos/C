21bea262aad0c3119627b86bee197aec09080409
leiltonramos <leiltonr@outlook.com>
2021-05-16 10:45:42 -0300

    learninC

--- compararnome.c
+++ compararnome.c
@@ -6,12 +6,12 @@ int main(){
     char nome1[15], nome2[15];
     int compare;
 
-    printf("Digite seu nome:\n>");
+    printf("Digite seu primeiro nome:\n>");
     fgets(nome1, 15, stdin);
 
     nome1[strcspn(nome1, "\n")]=0;
 
-    printf("Repita seu nome:\n>");
+    printf("Repita seu primeiro nome:\n>");
     fgets(nome2, 15, stdin);
 
     nome2[strcspn(nome2, "\n")]=0;
