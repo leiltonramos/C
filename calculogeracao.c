3a257621d5d642624bed21de3b333607fd55945a
leiltonramos <leiltonr@outlook.com>
2021-05-16 10:08:30 -0300

    learningC

---
+++ calculogeracao.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main(){
+
+    int idade;
+
+    printf("Digite sua idade:\n>");
+    scanf("%d", &idade);
+
+    if(idade <= 10){
+        printf("Vc eh geracao Alpha!\n");
+    }else{
+        if(idade <= 24){
+            printf("Vc eh geracao Z!\n");
+        }else{
+            if(idade <= 40){
+                printf("Vc eh geracao Y!\n");
+            }else{
+                if(idade <= 56){
+                    printf("Vc eh geracao X!\n");
+                }else{
+                    if(idade <= 75){
+                        printf("Vc eh geracao Boomer!\n");
+                    }else{
+                        if(idade <= 93){
+                            printf("Vc eh geracao Silent!\n");
+                        }else{
+                            if(idade >=94){
+                                printf("Vc eh centenario, PARABENS!\n");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return(0);
+}
---
+++ calculoidade.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    char nome[15];
+    int idade, anoNascimento;
+
+    printf("Digite seu nome:\n>");
+    fgets(nome, 15, stdin);
+
+    nome[strcspn(nome, "\n")]=0;
+
+    printf("Digite o ano de seu nascimento:\n>");
+    scanf("%d", &anoNascimento);
+
+    idade = 2021 - anoNascimento;
+
+    printf("Seu nome eh %s e sua idade eh %d anos!", nome, idade);
+
+    return(0);
+}
